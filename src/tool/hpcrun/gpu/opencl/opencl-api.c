3493e7842868af5122142a56bba21ef5bec3fd24
aarontcopal2 <aarontcopal2@gmail.com>
2021-03-02 18:04:25 +0000

    adding null check in isQueueInInOrderExecutionMode and areKernelParamsAliased functions in optimization-check.c

--- src/tool/hpcrun/gpu/opencl/opencl-api.c
+++ src/tool/hpcrun/gpu/opencl/opencl-api.c
@@ -1031,7 +1031,7 @@ hpcrun_clCreateCommandQueue
         properties,errcode_ret));
 
   if (optimization_check) {
-    isQueueInInOrderExecutionMode(properties);
+    isQueueInInOrderExecutionMode(&properties);
   }
 
   uint32_t context_id = opencl_cl_context_map_update((uint64_t)context);
@@ -1098,7 +1098,7 @@ hpcrun_clCreateCommandQueueWithProperties
   cl_command_queue queue = HPCRUN_OPENCL_CALL(clCreateCommandQueueWithProperties, (context, device, queue_properties, errcode_ret));
 
   if (optimization_check) {
-    isQueueInInOrderExecutionMode(*properties);
+    isQueueInInOrderExecutionMode(properties);
   }
 
   if (queue_properties != NULL) {
