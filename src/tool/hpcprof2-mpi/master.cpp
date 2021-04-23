200e4f9b73f871671c87e4d3bd244b26d6385c86
aarontcopal2 <aarontcopal2@gmail.com>
2021-04-23 19:13:57 +0000

    removing redundant files related to latency blaming

--- src/tool/hpcprof2-mpi/master.cpp
+++ src/tool/hpcprof2-mpi/master.cpp
@@ -56,7 +56,6 @@
 #include "lib/profile/sources/packed.hpp"
 #include "lib/profile/sinks/experimentxml4.hpp"
 #include "lib/profile/sinks/hpctracedb2.hpp"
-//#include "lib/profile/sinks/intel-advisor.hpp"
 #include "lib/profile/finalizers/denseids.hpp"
 #include "lib/profile/finalizers/directclassification.hpp"
 #include "lib/profile/finalizers/intel_def_use_graph.hpp"
