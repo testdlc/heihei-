0fcbaf294cc8a9e08926817e07a014a0bfbbeaa8
aarontcopal2 <aarontcopal2@gmail.com>
2021-05-11 02:20:57 +0000

    commenting memory free calls related to intel SIMD instrumentation. Code crashes for large applications with memory free (More investigation required)

--- src/tool/hpcrun/gpu/instrumentation/gtpin-instrumentation.c
+++ src/tool/hpcrun/gpu/instrumentation/gtpin-instrumentation.c
@@ -980,6 +980,7 @@ onKernelComplete
       }
     }
 
+#if 0
     // cleanup
     if (simd_knob) {
       curr_s = shead;
@@ -995,6 +996,7 @@ onKernelComplete
         curr_s = next_s;
       }
     }
+#endif
 
     // scalar simd loss
     uint64_t bb_total_simd_lanes = 0, scalar_simd_loss = 0;
